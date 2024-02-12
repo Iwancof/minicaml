a035c0a3a73ec707a783848ab5a781ea7a85d5c2
Iwancof <rock.iwanof@gmail.com>
2024-02-12 18:01:17 +0900

    remove extra comments

--- tests/test_recursive_function.ml
+++ tests/test_recursive_function.ml
@@ -12,14 +12,6 @@ let run str =
 let test_bool b =
   if b then "OK\n" else exit(1);;
 
-(*
-let rec f x = x in 0
-let rec f x = x in f 0
-let rec f x = if x = 0 then 1 else 2 + f (x + (- 1)) in f 0
-let rec f x = if x = 0 then 1 else x * f (x + (- 1)) in f 3
-let rec f x = if x = 0 then 1 else x * f (x + (- 1)) in f 5
-*)
-
 print_string (test_bool (run "let rec f x = x in 0" = IntVal 0));;
 print_string (test_bool (run "let rec f x = x in f 0" = IntVal 0));;
 print_string (test_bool (run "let rec f x = if x = 0 then 1 else 2 + f (x + (- 1)) in f 0" = IntVal 1));;
